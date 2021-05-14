71cb2bfbe55cb9feb36d09436947b91ecb7e4456
Sato Hayato <hayato.s.0328@gmail.com>
2021-05-14 07:16:24 +0000

    mynews

---
+++ resources/views/layouts/profile.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="Viewport" content="wigth=device-width, initial-svale=1">
+        
+        <!-- CSRF Token -->
+         {{-- 後の章で説明します --}}
+         <meta name="csrf-token" content="{{ csrf_token() }}">
+         
+        {{--各ページごとにtitleタグを入れるために@yieldで空けておきます。--}}
+        <title>@yield('tiitle')</title>
+        
+        <!-- Scripts -->
+         {{--Laravel標準で用意されているJavascriptを読み込みます --}}
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+        
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Ralewy:300,400,600" rel="stylesheet" type="text/css">
+        
+        <!-- Styles -->
+        {{-- Laravel標準で用意されているCSSを読み込みます --}}
+        <link herf="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        {{-- この章の後半で作成するCSSを読み込みます --}}
+        <link href="{{ secure_asset('css/profile.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            {{--　画面上部に表示するナビゲーションバーです。 --}}
+            <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
+                <div class="container">
+                    <a class="navbar-band" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-lavel="Toggle navigation">
+                       <span class="navbar-toggler-icon"></span>
+                    </button>
+                    
+                    <div class="collapse navbar-collpase" id="navbarSupportedContent">
+                        <!-- Left Side Of Navar -->
+                        <ul class="navbar-nav mr-auto">
+                            
+                            
+                        </ul>
+                        
+                        <!-- Right Side Of Navar -->
+                        <ul class="navbar-nav ml-auto">
+                            
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            {{-- ここまでナビゲーションバー --}}
+            
+            <main class="py-4">
+                {{-- コンテンツをここに入れるため、@yieldで空けておきます。 --}}
+                @yield('content')
+            </main>
+        </div>
+    </body>
+</html>
