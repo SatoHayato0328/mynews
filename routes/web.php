35c97a338e9787af1f28d3b3ce11bb495aafe9d3
Sato Hayato <hayato.s.0328@gmail.com>
2021-05-17 02:50:20 +0000

    No.21

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
 /*3.「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください
@@ -29,5 +29,21 @@ Route::get('xxx', 'Admin\AAAController@bbb');
 admin/profile/create にアクセスしたら ProfileController の add Action に、admin/profile/edit にアクセスしたら 
 ProfileController の edit Action に割り当てるように設定してください
 */
-Route::get('admin/profile/create', 'Admin\ProfileController@add');
-Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+Route::get('admin/profile/create', 'Admin\ProfileController@add')->middleware('auth');
+Route::get('admin/profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
